9032001480e834749f3daa0029f145477f96b81d
Guilherme <mttsguil@gmail.com>
2022-09-15 10:29:25 -0300

    add: desafio useReducer

--- src/views/examples/UseReducer.jsx
+++ src/views/examples/UseReducer.jsx
@@ -9,12 +9,22 @@ const initialState = {
     number: 0
 }
 
+// add 7, dividir 25, parse, addN
+
 function reducer(state, action) { 
     switch(action.type){ 
         case 'number_add2': 
         return {...state, number: state.number +2}
         case 'login':
             return {...state, user: { name: action.payload }}
+        case 'number_add7':
+            return {...state, number: state.number * 7}
+        case 'div25':
+            return {...state, number: state.number / 25}
+        case 'parse':
+            return {...state, number: parseInt(state.number)}
+        case 'addN':
+            return {...state, number: state.number + action.num}
         default:
             return state
     }
@@ -46,6 +56,22 @@ const UseReducer = (props) => {
                    </button>
                     <button onClick={e => dispatch({type: 'number_add2'})} className="btn">
                         +2
+                    </button>
+
+                    <button onClick={e => dispatch({type: 'number_add7'})} className="btn">
+                        *7
+                    </button>
+
+                    <button onClick={e => dispatch({type: 'div25'})} className="btn">
+                        /25
+                    </button>
+
+                    <button onClick={e => dispatch({type: 'parse'})} className="btn">
+                        Parse
+                    </button>
+
+                    <button onClick={e => dispatch({type: 'addN', num: 15})} className="btn">
+                        AddN
                     </button>
                 </div>
             </div>
